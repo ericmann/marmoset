e8fcf2a783147eb04210bf202977c1e95708efb2
Eric Mann <eric@eamann.com>
2016-06-02 21:17:26 -0700

    Start putting all of the pieces in place

---
+++ bin/marmoset.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+
+namespace EAMann\Marmoset;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+$application->add( new Command() );
+$application->run();
