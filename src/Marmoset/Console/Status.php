d3ad19b571a51af04abe7672f8f0377a338bcbcd
Eric Mann <eric@eamann.com>
2016-07-02 12:35:35 -0700

    PSR1/PSR2

--- src/Marmoset/Console/Status.php
+++ src/Marmoset/Console/Status.php
@@ -9,6 +9,7 @@
  */
 
 namespace EAMann\Marmoset\Console;
+
 use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -22,145 +23,145 @@ use Symfony\Component\Console\Output\OutputInterface;
 class Status
 {
 
-	private $redrawFreq = 1;
-	private $last;
+    private $redrawFreq = 1;
+    private $last;
 
-	/**
-	 * @var OutputInterface
-	 */
-	private $output;
-	private $startTime;
-	private $generation = 0;
-	private $generations_per_second = 0;
-	private $best = '';
-	private $formatLineCount;
-	private $overwrite = true;
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+    private $startTime;
+    private $generation = 0;
+    private $generations_per_second = 0;
+    private $best = '';
+    private $formatLineCount;
+    private $overwrite = true;
 
-	/**
-	 * Constructor.
-	 *
-	 * @param OutputInterface $output An OutputInterface instance
-	 * @param int             $max    Maximum steps (0 if unknown)
-	 */
-	public function __construct(OutputInterface $output, $max = 0)
-	{
-		if ($output instanceof ConsoleOutputInterface) {
-			$output = $output->getErrorOutput();
-		}
+    /**
+     * Constructor.
+     *
+     * @param OutputInterface $output An OutputInterface instance
+     * @param int $max Maximum steps (0 if unknown)
+     */
+    public function __construct(OutputInterface $output, $max = 0)
+    {
+        if ($output instanceof ConsoleOutputInterface) {
+            $output = $output->getErrorOutput();
+        }
 
-		$this->output = $output;
+        $this->output = $output;
 
-		if (!$this->output->isDecorated()) {
-			// disable overwrite when output does not support ANSI codes.
-			$this->overwrite = false;
-			// set a reasonable redraw frequency so output isn't flooded
-			$this->setRedrawFrequency($max / 10);
-		}
+        if (!$this->output->isDecorated()) {
+            // disable overwrite when output does not support ANSI codes.
+            $this->overwrite = false;
+            // set a reasonable redraw frequency so output isn't flooded
+            $this->setRedrawFrequency($max / 10);
+        }
 
-		$this->last = $this->startTime = time();
-		$this->generation = 0;
-		$this->generations_per_second = 0;
-		$this->best = '';
-	}
+        $this->last = $this->startTime = time();
+        $this->generation = 0;
+        $this->generations_per_second = 0;
+        $this->best = '';
+    }
 
-	/**
-	 * Gets the output start time.
-	 *
-	 * @return int The output start time
-	 */
-	public function getStartTime()
-	{
-		return $this->startTime;
-	}
+    /**
+     * Gets the output start time.
+     *
+     * @return int The output start time
+     */
+    public function getStartTime()
+    {
+        return $this->startTime;
+    }
 
-	/**
-	 * Sets the redraw frequency.
-	 *
-	 * @param int|float $freq The frequency in steps
-	 */
-	public function setRedrawFrequency($freq)
-	{
-		$this->redrawFreq = max((int) $freq, 1);
-	}
+    /**
+     * Sets the redraw frequency.
+     *
+     * @param int|float $freq The frequency in steps
+     */
+    public function setRedrawFrequency($freq)
+    {
+        $this->redrawFreq = max((int)$freq, 1);
+    }
 
-	/**
-	 * Starts the output.
-	 *
-	 * @param string [$best] Optional pre-population of the best attempt
-	 */
-	public function start( $best = '' )
-	{
-		$this->last = $this->startTime = time();
-		$this->generation = 0;
-		$this->generations_per_second = 0;
-		$this->best = $best;
+    /**
+     * Starts the output.
+     *
+     * @param string [$best] Optional pre-population of the best attempt
+     */
+    public function start($best = '')
+    {
+        $this->last = $this->startTime = time();
+        $this->generation = 0;
+        $this->generations_per_second = 0;
+        $this->best = $best;
 
-		$this->display();
-	}
+        $this->display();
+    }
 
-	/**
-	 * Sets whether to overwrite the status, false for new line.
-	 *
-	 * @param bool $overwrite
-	 */
-	public function setOverwrite( $overwrite )
-	{
-		$this->overwrite = (bool) $overwrite;
-	}
+    /**
+     * Sets whether to overwrite the status, false for new line.
+     *
+     * @param bool $overwrite
+     */
+    public function setOverwrite($overwrite)
+    {
+        $this->overwrite = (bool)$overwrite;
+    }
 
-	/**
-	 * Update the current generation count and generation rate.
-	 *
-	 * @param int $generation
-	 * 
-	 * @return Status
-	 */
-	public function setGeneration( $generation )
-	{
-		$now = time();
-		$this->generation = (int) $generation;
+    /**
+     * Update the current generation count and generation rate.
+     *
+     * @param int $generation
+     *
+     * @return Status
+     */
+    public function setGeneration($generation)
+    {
+        $now = time();
+        $this->generation = (int)$generation;
 
-		if ( $now > $this->startTime ) {
-			$this->generations_per_second = floor( $generation / ( $now - $this->startTime ) );
-		}
+        if ($now > $this->startTime) {
+            $this->generations_per_second = floor($generation / ($now - $this->startTime));
+        }
 
-		if ( $now > ( $this->last + $this->redrawFreq ) ) {
-			$this->display();
-		}
-		
-		return $this;
-	}
+        if ($now > ($this->last + $this->redrawFreq)) {
+            $this->display();
+        }
 
-	/**
-	 * Set the monkeys' best guess at writing Shakespeare.
-	 *
-	 * @param string $best
-	 * 
-	 * @return Status
-	 */
-	public function setBest( $best )
-	{
-		$this->best = $best;
+        return $this;
+    }
 
-		if ( time() > ( $this->last + $this->redrawFreq ) ) {
-			$this->display();
-		}
-		
-		return $this;
-	}
+    /**
+     * Set the monkeys' best guess at writing Shakespeare.
+     *
+     * @param string $best
+     *
+     * @return Status
+     */
+    public function setBest($best)
+    {
+        $this->best = $best;
 
-	/**
-	 * Outputs the current status string.
-	 */
-	public function display()
-	{
-		static $header = false;
+        if (time() > ($this->last + $this->redrawFreq)) {
+            $this->display();
+        }
 
-		$this->last = time();
-		$status = '';
+        return $this;
+    }
 
-		if ( ! $header ) {
-			$status .= <<<PHP
+    /**
+     * Outputs the current status string.
+     */
+    public function display()
+    {
+        static $header = false;
+
+        $this->last = time();
+        $status = '';
+
+        if (!$header) {
+            $status .= <<<PHP
   __  __             _                    _         _   _            __  __            _     _            
  |  \/  |           | |                  (_)       | | | |          |  \/  |          | |   (_)           
  | \  / | ___  _ __ | | _____ _   _ ___   _ _ __   | |_| |__   ___  | \  / | __ _  ___| |__  _ _ __   ___ 
@@ -194,58 +195,59 @@ class Status
 |   Current                                                                   | Generation |  Gen / sec  |
 |-----------------------------------------------------------------------------|------------|-------------|\n
 PHP;
-			$header = true;
-		}
+            $header = true;
+        }
 
-		$gen = (string) $this->generation;
-		$gps = (string) $this->generations_per_second;
+        $gen = (string)$this->generation;
+        $gps = (string)$this->generations_per_second;
 
-		// Build the status string
-		$status .= "|" . str_repeat( ' ', 77 ) . "|" . str_repeat( ' ', 11 - strlen( $gen ) ) .$gen . " |" . str_repeat( ' ', 12 - strlen( $gps ) ) . $gps . " |\n";
+        // Build the status string
+        $status .= "|" . str_repeat(' ', 77) . "|" . str_repeat(' ', 11 - strlen($gen)) . $gen . " |" . str_repeat(' ',
+                12 - strlen($gps)) . $gps . " |\n";
 
-		// Make sure we can overwrite previous lines
-		$this->formatLineCount = 1;
+        // Make sure we can overwrite previous lines
+        $this->formatLineCount = 1;
 
-		// Overwrite all the things
-		$this->overwrite( $status );
-	}
+        // Overwrite all the things
+        $this->overwrite($status);
+    }
 
-	/**
-	 * Removes the status from the current line.
-	 *
-	 * Call display() to show the status again.
-	 */
-	public function clear()
-	{
-		if (!$this->overwrite) {
-			return;
-		}
+    /**
+     * Removes the status from the current line.
+     *
+     * Call display() to show the status again.
+     */
+    public function clear()
+    {
+        if (!$this->overwrite) {
+            return;
+        }
 
-		$this->overwrite('');
-	}
+        $this->overwrite('');
+    }
 
-	/**
-	 * Overwrites a previous message to the output.
-	 *
-	 * @param string $message The message
-	 */
-	private function overwrite( $message )
-	{
-		if ( $this->overwrite ) {
-			// Move the cursor to the beginning of the line
-			$this->output->write( "\x0D" );
+    /**
+     * Overwrites a previous message to the output.
+     *
+     * @param string $message The message
+     */
+    private function overwrite($message)
+    {
+        if ($this->overwrite) {
+            // Move the cursor to the beginning of the line
+            $this->output->write("\x0D");
 
-			// Erase the line
-			$this->output->write( "\x1B[2K" );
+            // Erase the line
+            $this->output->write("\x1B[2K");
 
-			// Erase previous lines
-			if ( $this->formatLineCount > 0 ) {
-				$this->output->write( str_repeat( "\x1B[1A\x1B[2K", $this->formatLineCount ) );
-			}
-		} else {
-			$this->output->writeln( '' );
-		}
+            // Erase previous lines
+            if ($this->formatLineCount > 0) {
+                $this->output->write(str_repeat("\x1B[1A\x1B[2K", $this->formatLineCount));
+            }
+        } else {
+            $this->output->writeln('');
+        }
 
-		$this->output->write($message);
-	}
+        $this->output->write($message);
+    }
 }
