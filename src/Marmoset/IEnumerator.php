25ebc1466129e3fbb43f83877a7ddba65c427b26
Eric Mann <eric@eamann.com>
2016-07-18 21:53:22 -0700

    Add enumeration scaffolds

---
+++ src/Marmoset/IEnumerator.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the Marmoset project.
+ *
+ * (c) Eric Mann <eric@eamann.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EAMann\Marmoset;
+
+interface IEnumerator
+{
+
+}
